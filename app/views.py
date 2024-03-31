d841e75f324f7f43cf35dfeac1e8e9c23d00ac9f
kantanatp <kp41@illinois.edu>
2024-03-30 20:51:06 -0400

    v closet template

---
+++ app/views.py
@@ -0,0 +1,28 @@
+from . import app, db
+from flask import render_template, request, redirect, url_for, flash, jsonify
+from .models import Product, Retailer, User, VirtualCloset
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/clothing')
+def clothing():
+    return render_template('clothing.html')
+
+@app.route('/retailers')
+def retailers():
+    return render_template('retailers.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/virtual_closet')
+def virtual_closet():
+    return render_template('virtual_closet.html')
+
+@app.route('/account')
+def account():
+    return render_template('account.html')
+
