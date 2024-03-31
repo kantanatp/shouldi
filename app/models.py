8f798b46a5f79aa1a3f31a8f143a6bafad800f91
kantanatp <kp41@illinois.edu>
2024-03-31 06:12:38 -0400

    add images

--- app/models.py
+++ app/models.py
@@ -9,6 +9,7 @@ class Retailer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     retailer_name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(255))
+    image_path = db.Column(db.String(255))
     
     # Establish a relationship with Product
     products = db.relationship('Product', backref='retailer', lazy=True)
